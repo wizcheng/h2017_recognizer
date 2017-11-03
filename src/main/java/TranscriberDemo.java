49dc166e3f381b244496d865a528a6056aa938b7
wizcheng <wizcheng@gmail.com>
2017-11-03 22:09:53 +0800

    Initial commit

---
+++ src/main/java/TranscriberDemo.java
@@ -0,0 +1,33 @@
+
+import edu.cmu.sphinx.api.Configuration;
+import edu.cmu.sphinx.api.SpeechResult;
+import edu.cmu.sphinx.api.StreamSpeechRecognizer;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+public class TranscriberDemo {
+
+    public static void main(String[] args) throws Exception {
+
+        Configuration configuration = new Configuration();
+
+        configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
+        configuration.setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
+        configuration.setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin");
+
+        configuration.setSampleRate(8000);
+
+        StreamSpeechRecognizer recognizer = new StreamSpeechRecognizer(configuration);
+//        InputStream stream = new FileInputStream(new File("10001-90210-01803.wav"));
+        InputStream stream = new FileInputStream(new File("voice_samples/OSR_us_000_0034_8k.wav"));
+
+        recognizer.startRecognition(stream);
+        SpeechResult result;
+        while ((result = recognizer.getResult()) != null) {
+            System.out.format("Hypothesis: %s\n", result.getHypothesis());
+        }
+        recognizer.stopRecognition();
+    }
+}
